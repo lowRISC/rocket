6a194e3906be46aececa6b7db811ec08674ed3c4
Wei Song <wsong83@gmail.com>
2017-01-27 16:19:36 +0000

    add TAGR/TAGW into instruction decoder

--- src/main/scala/idecode.scala
+++ src/main/scala/idecode.scala
@@ -313,6 +313,6 @@ class TagDecode(implicit val p: Parameters) extends DecodeConstants
                 //   |     | | | | | | | |       |       |      |      |         | |         |     | | | |     | | | | fence
                 //   |     | | | | | | | |       |       |      |      |         | |         |     | | | |     | | | | | amo
                 //   |     | | | | | | | |       |       |      |      |         | |         |     | | | |     | | | | | |
-    LTAG->      List(xpr64,N,N,N,N,N,N,Y,A2_IMM, A1_RS1, IMM_I, DW_XPR,FN_ADD,   Y,M_XRD,    MT_T, N,N,N,N,N,Y,CSR.N,N,N,N),
-    STAG->      List(xpr64,N,N,N,N,N,Y,Y,A2_IMM, A1_RS1, IMM_S, DW_XPR,FN_ADD,   Y,M_XWR,    MT_T, N,N,N,N,N,N,CSR.N,N,N,N))
+    TAGR->      List(xpr64,N,N,N,N,N,N,Y,A2_IMM, A1_RS1, IMM_I, DW_XPR,FN_ADD,   Y,M_XRD,    MT_T, N,N,N,N,N,Y,CSR.N,N,N,N),
+    TAGW->      List(xpr64,N,N,N,N,N,Y,Y,A2_IMM, A1_RS1, IMM_S, DW_XPR,FN_ADD,   Y,M_XWR,    MT_T, N,N,N,N,N,N,CSR.N,N,N,N))
 }
